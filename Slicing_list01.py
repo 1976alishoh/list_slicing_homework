b4adc4d5a93838b56673b5099199516718b76597
1976alishoh <106156697+iqrobocreator@users.noreply.github.com>
2022-11-09 09:08:36 +0300

    01 done

--- Slicing_list01.py
+++ Slicing_list01.py
@@ -6,4 +6,13 @@ def main(numbers):
     Returns:
         list: return answer.
     """
-    return
+    i = 0
+    l = []
+    while i < len(numbers):
+        if i % 2 ==0:
+            l.append(numbers[i])
+        
+        i = i +1
+    return l
+print(main([1, 2, 3, 4, 5]))
+print(main([0, 1, 2, 3, 4, 5]))
